3d51460d9ab33198869aa52c8824b671f69aea78
姚旭 <1453810050@qq.com>
2025-12-02 19:47:56 +0800

    codable 相关的便利方法

---
+++ JustKit/Src/Utils/Category/Codable+Default.swift
@@ -0,0 +1,54 @@
+//
+//  Created by 姚旭 on 2025/12/2.
+//
+
+import Foundation
+
+extension KeyedDecodingContainer {
+    
+    func decode<T>(
+        _ type: T.Type,
+        forKey key: KeyedDecodingContainer<K>.Key,
+        defalut: T
+    ) throws -> T where T : Decodable {
+        let value = try? decode(type, forKey: key)
+        return value ?? defalut
+    }
+    
+    func decodeIfPresent<T>(
+        _ type: T.Type,
+        forKey key: K,
+        defalut: T?
+    ) throws -> T? where T? : Decodable {
+        let value = try? decodeIfPresent(type, forKey: key)
+        return value ?? defalut
+    }
+    
+}
+
+extension Encodable {
+    
+    var jsonData: Data? {
+        try? JSONEncoder().encode(self)
+    }
+    
+    var jsonString: String? {
+        jsonData.flatMap { data in
+            String(data: data, encoding: .utf8)
+        }
+    }
+    
+}
+
+extension Decodable {
+    
+    static func decode(from jsonData: Data) -> Self?  {
+        return try? JSONDecoder().decode(self, from: jsonData)
+    }
+    
+    static func decode(from jsonString: String) -> Self?  {
+        guard let jsonData = jsonString.data(using: .utf8) else { return nil }
+        return try? JSONDecoder().decode(self, from: jsonData)
+    }
+    
+}
