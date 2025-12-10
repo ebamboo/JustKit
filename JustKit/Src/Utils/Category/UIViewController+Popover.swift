d7a1b75acff3de9ce94ae4eb393a21f2d8b7eaeb
姚旭 <1453810050@qq.com>
2025-12-10 20:11:55 +0800

    showPopoverMenu 优化

--- JustKit/Src/Utils/Category/UIViewController+Popover.swift
+++ JustKit/Src/Utils/Category/UIViewController+Popover.swift
@@ -6,15 +6,21 @@ import UIKit
 
 public extension UIViewController {
     
-    /// popoverBackgroundViewClass 参数若为空时，使用系统默认的 popover 样式；
-    /// 通过设置该参数为 NoneArrowPopoverBackgroundView 类型可以达到隐藏箭头的功能；
-    func showPopoverMenu(_ popoverViewController: UIViewController,
-                         sourceView: UIView,
-                         permittedArrowDirections: UIPopoverArrowDirection = .any,
-                         popoverBackgroundViewClass: (any UIPopoverBackgroundViewMethods.Type)? = nil) {
+    /// popoverBackgroundViewClass 参数为空时，使用系统默认的 popover 样式；
+    /// 若为 NoneArrowPopoverBackgroundView 类型时，可以达到隐藏箭头的效果；
+    func showPopoverMenu(
+        _ popoverViewController: UIViewController,
+        sourceView: UIView,
+        sourceRect: CGRect? = nil, // 不为 nil 时，屏幕旋转和缩放时不会自动适配和恢复
+        permittedArrowDirections: UIPopoverArrowDirection = .any,
+        popoverBackgroundViewClass: (any UIPopoverBackgroundViewMethods.Type)? = nil
+    ) {
         self.popoverPresentationControllerDelegate = PopoverPresentationControllerDelegate()
         popoverViewController.modalPresentationStyle = .popover
         popoverViewController.popoverPresentationController?.sourceView = sourceView
+        if let sourceRect {
+            popoverViewController.popoverPresentationController?.sourceRect = sourceRect
+        }
         popoverViewController.popoverPresentationController?.permittedArrowDirections = permittedArrowDirections
         popoverViewController.popoverPresentationController?.popoverBackgroundViewClass = popoverBackgroundViewClass
         popoverViewController.popoverPresentationController?.delegate = self.popoverPresentationControllerDelegate
