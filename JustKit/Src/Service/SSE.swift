c3345254d98471e5f3e7496a1ba5cc234134c9ea
姚旭 <1453810050@qq.com>
2025-12-24 19:54:39 +0800

    精简优化SSE 网络工具

--- JustKit/Src/Service/SSE.swift
+++ JustKit/Src/Service/SSE.swift
@@ -4,9 +4,12 @@
 
 import Foundation
 
-struct SSE {
+public class SSESession: NSObject, URLSessionDataDelegate {
     
-    /// a static method request for initiating SSE connections:
+    /// shared instance
+    public static let shared = SSESession()
+    
+    /// a instance method request for initiating SSE connections:
     /// - Parameters:
     ///   - url: The endpoint for SSE.
     ///   - headers: Custom headers for the request.
@@ -15,43 +18,20 @@ struct SSE {
     ///   - completionHandler: Called when the task completes, with or without error.
     /// - Returns: A URLSessionDataTask that represents the ongoing request.
     @discardableResult
-    static func request(with url: URL,
-                        headers: [String: String] = [:],
-                        requestModifier: ((inout URLRequest) -> Void)? = nil,
-                        eventHandler: @escaping (URLSessionDataTask, SSEEvent) -> Void,
-                        completionHandler: @escaping (URLSessionDataTask, Error?) -> Void)
-    -> URLSessionDataTask {
-        let task = SSESession.shared.request(with: url,
-                                             headers: headers,
-                                             requestModifier: requestModifier,
-                                             eventHandler: eventHandler,
-                                             completionHandler: completionHandler)
-        return task
-    }
-    
-}
-
-fileprivate class SSESession: NSObject, URLSessionDataDelegate {
-    
-    static let shared = SSESession()
-    
-    private lazy var session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-    private var workList: [Int: SSESessionWork] = [:]
-    
-    @discardableResult
-    func request(with url: URL,
-                 headers: [String: String] = [:],
-                 requestModifier: ((inout URLRequest) -> Void)? = nil,
-                 eventHandler: @escaping (URLSessionDataTask, SSEEvent) -> Void,
-                 completionHandler: @escaping (URLSessionDataTask, Error?) -> Void)
-    -> URLSessionDataTask {
+    public func dataTask(
+        with url: URL,
+        headers: [String: String] = [:],
+        requestModifier: ((inout URLRequest) -> Void)? = nil,
+        eventHandler: @escaping (URLSessionDataTask, SSEEvent) -> Void,
+        completionHandler: @escaping (URLSessionDataTask, Error?) -> Void
+    ) -> URLSessionDataTask {
         var request = URLRequest(url: url)
-        requestModifier?(&request)
         request.httpMethod = "GET"
         request.setValue("text/event-stream", forHTTPHeaderField: "Accept")
         headers.forEach { key, value in
             request.setValue(value, forHTTPHeaderField: key)
         }
+        requestModifier?(&request)
         let task = session.dataTask(with: request)
         let work = SSESessionWork(onEvent: eventHandler, onCompletion: completionHandler)
         workList[task.taskIdentifier] = work
@@ -59,20 +39,18 @@ fileprivate class SSESession: NSObject, URLSessionDataDelegate {
         return task
     }
     
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+    private lazy var session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+    private var workList: [Int: SSESessionWork] = [:]
+    
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         guard let work = workList[dataTask.taskIdentifier] else { return }
         work.didReceive(data: data, dataTask: dataTask)
     }
     
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: (any Error)?) {
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: (any Error)?) {
         guard let work = workList[task.taskIdentifier] else { return }
         work.didComplete(with: error, dataTask: task)
         workList.removeValue(forKey: task.taskIdentifier)
-        
-        
-        /*if let response = task.response as? HTTPURLResponse, response.statusCode == 401 {
-            // 统一处理未登录情况
-        }*/
     }
     
 }
@@ -85,8 +63,10 @@ fileprivate class SSESessionWork {
     private let lock = NSLock()
     private var buffer = Data()
     
-    init(onEvent: @escaping (URLSessionDataTask, SSEEvent) -> Void,
-         onCompletion: @escaping (URLSessionDataTask, Error?) -> Void) {
+    init(
+        onEvent: @escaping (URLSessionDataTask, SSEEvent) -> Void,
+        onCompletion: @escaping (URLSessionDataTask, Error?) -> Void
+    ) {
         self.onEvent = onEvent
         self.onCompletion = onCompletion
     }
