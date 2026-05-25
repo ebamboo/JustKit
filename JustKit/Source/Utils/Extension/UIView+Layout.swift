4f2eb53fe2a4e9889009ed99d91361e441b6af6d
姚旭 <1453810050@qq.com>
2026-05-26 00:59:24 +0800

    优化 uiview +  layout 实现风格

--- JustKit/Source/Utils/Extension/UIView+Layout.swift
+++ JustKit/Source/Utils/Extension/UIView+Layout.swift
@@ -9,66 +9,38 @@ import UIKit
 public extension UIView {
     
     var top: CGFloat {
-        get {
-            return frame.origin.y
-        }
-        set {
-            frame.origin.y = newValue
-        }
+        get { frame.origin.y }
+        set { frame.origin.y = newValue }
     }
 
     var left: CGFloat {
-        get {
-            return frame.origin.x
-        }
-        set {
-            frame.origin.x = newValue
-        }
+        get { frame.origin.x }
+        set { frame.origin.x = newValue }
     }
 
     var bottom: CGFloat {
-        get {
-            return frame.origin.y + frame.size.height
-        }
-        set {
-            frame.origin.y = newValue - frame.size.height
-        }
+        get { frame.origin.y + frame.size.height }
+        set { frame.origin.y = newValue - frame.size.height }
     }
 
     var right: CGFloat {
-        get {
-            return frame.origin.x + frame.size.width
-        }
-        set {
-            frame.origin.x = newValue - frame.size.width
-        }
+        get { frame.origin.x + frame.size.width }
+        set { frame.origin.x = newValue - frame.size.width }
     }
 
     var centerX: CGFloat {
-        get {
-            return center.x
-        }
-        set {
-            center.x = newValue
-        }
+        get { center.x }
+        set { center.x = newValue }
     }
 
     var centerY: CGFloat {
-        get {
-            return center.y
-        }
-        set {
-            center.y = newValue
-        }
+        get { center.y }
+        set { center.y = newValue }
     }
 
     var origin: CGPoint {
-        get {
-            return frame.origin
-        }
-        set {
-            frame.origin = newValue
-        }
+        get { frame.origin }
+        set { frame.origin = newValue }
     }
     
 }
@@ -78,30 +50,18 @@ public extension UIView {
 public extension UIView {
 
     var width: CGFloat {
-        get {
-            return frame.size.width
-        }
-        set {
-            frame.size.width = newValue
-        }
+        get { frame.size.width }
+        set { frame.size.width = newValue }
     }
 
     var height: CGFloat {
-        get {
-            return frame.size.height
-        }
-        set {
-            frame.size.height = newValue
-        }
+        get { frame.size.height }
+        set { frame.size.height = newValue }
     }
 
     var size: CGSize {
-        get {
-            return frame.size
-        }
-        set {
-            frame.size = newValue
-        }
+        get { frame.size }
+        set { frame.size = newValue }
     }
 
 }
