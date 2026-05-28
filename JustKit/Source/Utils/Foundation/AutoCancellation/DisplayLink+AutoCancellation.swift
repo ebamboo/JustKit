e3e9136aad1045f04c8af5eefe127e715c140aaf
ebamboo <49512723+ebamboo@users.noreply.github.com>
2026-05-28 16:29:49 +0800

    Update DisplayLink+AutoCancellation.swift
    移除取消之外的所以功能

--- JustKit/Source/Utils/Foundation/AutoCancellation/DisplayLink+AutoCancellation.swift
+++ JustKit/Source/Utils/Foundation/AutoCancellation/DisplayLink+AutoCancellation.swift
@@ -4,43 +4,6 @@
 
 import UIKit
 
-// MARK: - 闭包初始化
-
-///
-/// 将 target/selector 模式转化为闭包形式，使调用方可通过 `[weak self]` 主动打破循环引用。
-///
-/// ## 示例
-/// ```swift
-/// let link = CADisplayLink { [weak self] displayLink in
-///     self?.update(displayLink)
-/// }
-/// link.add(to: .main, forMode: .common)
-/// link.store(on: self)
-/// ```
-///
-public extension CADisplayLink {
-    
-    /// 以闭包形式初始化 CADisplayLink，替代 target/selector 模式
-    convenience init(handler: @escaping (CADisplayLink) -> Void) {
-        self.init(
-            target: DisplayLinkTarget(handler: handler),
-            selector: #selector(DisplayLinkTarget.invoke(_:))
-        )
-    }
-    
-    /// CADisplayLink 的 target 对象，将 action 转发给闭包
-    private class DisplayLinkTarget: NSObject {
-        let handler: (CADisplayLink) -> Void
-        init(handler: @escaping (CADisplayLink) -> Void) {
-            self.handler = handler
-        }
-        @objc func invoke(_ sender: CADisplayLink) {
-            handler(sender)
-        }
-    }
-    
-}
-
 // MARK: - 自动取消
 
 public extension CADisplayLink {
