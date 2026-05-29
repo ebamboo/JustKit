ee34f961db41a15453b4a6b64e219c4f4835b239
ebamboo <49512723+ebamboo@users.noreply.github.com>
2026-05-29 14:32:28 +0800

    Update UnevenCornerView.swift

--- JustKit/Source/Utils/Foundation/UnevenCornerView.swift
+++ JustKit/Source/Utils/Foundation/UnevenCornerView.swift
@@ -28,31 +28,26 @@ public class UnevenCornerView: UIView {
     private var maskPath: UIBezierPath {
         let rect = bounds
         let path = UIBezierPath()
-        
         if topLeftRadius > 0.0 {
             path.addArc(withCenter: CGPoint(x: topLeftRadius, y: topLeftRadius), radius: topLeftRadius, startAngle: -Double.pi, endAngle: -Double.pi/2, clockwise: true)
         } else {
             path.move(to: CGPoint(x: 0, y: 0))
         }
-        
         if topRightRadius > 0.0 {
             path.addArc(withCenter: CGPoint(x: rect.width-topRightRadius, y: topRightRadius), radius: topRightRadius, startAngle: -Double.pi/2, endAngle: 0, clockwise: true)
         } else {
             path.addLine(to: CGPoint(x: rect.width, y: 0))
         }
-        
         if bottomRightRadius > 0.0 {
             path.addArc(withCenter: CGPoint(x: rect.width-bottomRightRadius, y: rect.height-bottomRightRadius), radius: bottomRightRadius, startAngle: 0, endAngle: Double.pi/2, clockwise: true)
         } else {
             path.addLine(to: CGPoint(x: rect.width, y: rect.height))
         }
-        
         if bottomLeftRadius > 0.0 {
             path.addArc(withCenter: CGPoint(x: bottomLeftRadius, y: rect.height-bottomLeftRadius), radius: bottomLeftRadius, startAngle: Double.pi/2, endAngle: Double.pi, clockwise: true)
         } else {
             path.addLine(to: CGPoint(x: 0, y: rect.height))
         }
-        
         path.close()
         return path
     }
