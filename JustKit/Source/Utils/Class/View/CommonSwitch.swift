97bba2c2e258fd92fd10c474b0815c5e4fc35ea3
姚旭 <1453810050@qq.com>
2026-06-04 22:35:54 +0800

    自定义开关控件： 初步重构

--- JustKit/Source/Utils/Class/View/CommonSwitch.swift
+++ JustKit/Source/Utils/Class/View/CommonSwitch.swift
@@ -4,114 +4,111 @@
 
 import UIKit
 
-///  仿 UISwitch 样式的自定义开关控件：
+/// 仿 UISwitch 样式的自定义开关控件：
 /// - 支持自定义圆钮颜色
-/// - 支持自定义开/关状态颜色（UISwitch不支持关状态颜色）
-/// - 支持修改控件高度和宽度（UISwitch不支持修改尺寸）
+/// - 支持自定义开/关状态颜色（UISwitch 不支持关状态颜色）
+/// - 支持修改控件高度和宽度（UISwitch 不支持修改尺寸）
 ///
-///  通过 UIControl.Event.valueChanged 来处理点击切换事件
-public class CommonSwitch: UIControl {
+/// 通过 `UIControl.Event.valueChanged` 来处理点击切换事件
+public final class ToggleSwitch: UIControl {
     
-    // MARK: public
+    // MARK: - 配置属性
     
     /// 控件是否开启
-    @IBInspectable public var isOn: Bool = false {
+    @IBInspectable public var isOn: Bool = true {
         didSet {
             updateThumbPosition()
-            updateColors()
+            updateAppearance()
         }
     }
     
-    /// 两次有效点击之间最小时间间隔（单位毫秒，0表示不设置最小时间间隔）
-    @IBInspectable public var minTimeInterval: Int = 0
+    /// 两次有效点击之间最小时间间隔（单位秒，0 表示不设置最小时间间隔）
+    @IBInspectable public var debounceInterval: TimeInterval = 0
     
     /// 开启时的背景颜色
     @IBInspectable public var onTintColor: UIColor = .systemGreen {
-        didSet {
-            updateColors()
-        }
+        didSet { updateAppearance() }
     }
     
     /// 关闭时的背景颜色
     @IBInspectable public var offTintColor: UIColor = .init(red: 120/255, green: 120/255, blue: 128/255, alpha: 0.16) {
-        didSet {
-            updateColors()
-        }
+        didSet { updateAppearance() }
     }
     
     /// 圆钮的颜色
     @IBInspectable public var thumbTintColor: UIColor = .white {
-        didSet {
-            updateColors()
-        }
+        didSet { updateAppearance() }
     }
     
-    // MARK: ui
+    private let padding: CGFloat = 2
     
-    private let backgroundView = UIView()
-    private let thumbView = UIView()
+    // MARK: - Components
     
-    // MARK: life
+    private lazy var trackView = {
+        let v = UIView()
+        v.isUserInteractionEnabled = false
+        v.backgroundColor = isOn ? onTintColor : offTintColor
+        self.addSubview(v)
+        return v
+    }()
     
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
-    }
+    private lazy var thumbView = {
+        let v = UIView()
+        v.isUserInteractionEnabled = false
+        v.backgroundColor = thumbTintColor
+        self.addSubview(v)
+        return v
+    }()
     
-    public required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        commonInit()
-    }
-    
-    private func commonInit() {
-        // 设置初始状态
-        addSubview(backgroundView)
-        addSubview(thumbView)
-        updateColors()
-        updateThumbPosition()
-        // 添加手势识别
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapAction))
-        addGestureRecognizer(tapGesture)
-    }
+    // MARK: - Override
     
     public override func layoutSubviews() {
         super.layoutSubviews()
-        // 更新背景视图frame
-        backgroundView.frame = bounds
-        backgroundView.layer.cornerRadius = bounds.height / 2
-        // 更新圆钮视图frame
-        let thumbSize = CGSize(width: bounds.height - 4, height: bounds.height - 4)
-        thumbView.frame = CGRect(origin: .zero, size: thumbSize)
-        thumbView.layer.cornerRadius = thumbSize.height / 2
-        // 更新圆钮位置
-        updateThumbPosition()
+        // 设置 trackView 的 frame 及圆角。
+        trackView.frame = bounds
+        trackView.layer.cornerRadius = bounds.height / 2
+        // 设置 thumbView 的 frame 及圆角。
+        let thumbSide = bounds.height - 2 * padding
+        let thumbMinX = padding
+        let thumbMaxX = bounds.width - thumbSide - padding
+        thumbView.frame = .init(
+            x: isOn ? thumbMaxX : thumbMinX,
+            y: padding,
+            width: thumbSide,
+            height: thumbSide
+        )
+        thumbView.layer.cornerRadius = thumbSide / 2
     }
     
-    private func updateColors() {
-        backgroundView.backgroundColor = isOn ? onTintColor : offTintColor
+    public override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        return true
+    }
+    
+    public override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        super.endTracking(touch, with: event)
+        guard let touch = touch,
+              bounds.contains(touch.location(in: self)) else { return }
+        isOn.toggle()
+        sendActions(for: .valueChanged)
+        if debounceInterval > 0 {
+            isEnabled = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + debounceInterval) { [weak self] in
+                self?.isEnabled = true
+            }
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func updateAppearance() {
+        trackView.backgroundColor = isOn ? onTintColor : offTintColor
         thumbView.backgroundColor = thumbTintColor
     }
     
     private func updateThumbPosition() {
-        let padding: CGFloat = 2
-        let maxX = bounds.width - thumbView.bounds.width - padding
         let minX = padding
-        let targetX = isOn ? maxX : minX
-        thumbView.frame.origin.x = targetX
-        thumbView.frame.origin.y = padding
-    }
-    
-    // MARK: action
-    
-    @objc private func didTapAction() {
-        isOn.toggle()
-        sendActions(for: .valueChanged)
-        if minTimeInterval > 0 {
-            isUserInteractionEnabled = false
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(minTimeInterval)) { [weak self] in
-                self?.isUserInteractionEnabled = true
-            }
-        }
+        let maxX = bounds.width - thumbView.bounds.width - padding
+        thumbView.frame.origin.x = isOn ? maxX : minX
     }
     
 }
