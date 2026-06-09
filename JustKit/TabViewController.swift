bd8f9edd4319d5e2f0d8b0d718a3dae182022d42
姚旭 <1453810050@qq.com>
2026-06-09 21:35:43 +0800

    tab顺序调整

--- JustKit/TabViewController.swift
+++ JustKit/TabViewController.swift
@@ -9,9 +9,9 @@ class TabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let vcInfoList: [(vc: UIViewController, title: String)] = [
-            (ExperienceHomeViewController(), "经验方案"),
-            (ServiceHomeViewController(), "项目工具"),
             (UtilsHomeViewController(), "通用工具"),
+            (ServiceHomeViewController(), "项目工具"),
+            (ExperienceHomeViewController(), "经验方案"),
         ]
         vcInfoList.forEach { couple in
             couple.vc.tabBarItem = UITabBarItem(title: couple.title, image: nil, selectedImage: nil)
