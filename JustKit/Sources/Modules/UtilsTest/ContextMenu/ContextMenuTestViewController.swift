e71828d5d00035350b83852083678a2b0d149dd6
姚旭 <1453810050@qq.com>
2026-07-17 21:04:50 +0800

    测试编辑菜单

--- JustKit/Sources/Modules/UtilsTest/ContextMenu/ContextMenuTestViewController.swift
+++ JustKit/Sources/Modules/UtilsTest/ContextMenu/ContextMenuTestViewController.swift
@@ -23,10 +23,31 @@ class ContextMenuTestViewController: UIViewController {
         }
     }
     
+    
+    @IBOutlet weak var testEditLabel: UILabel! {
+        didSet {
+            if #available(iOS 16.0, *) {
+                testEditLabel.editMenu = testEditMenu
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+    
     lazy var testMenu = {
         let edit = UIAction(title: "编辑") { _ in print("编辑") }
         let delete = UIAction(title: "删除", attributes: .destructive) { _ in print("删除") }
         let menu = UIMenu(children: [edit, delete])
+        return menu
+    }()
+    
+    lazy var testEditMenu = {
+        let copy = UIAction(title: "自定义拷贝") { _ in print("拷贝") }
+        let select = UIAction(title: "自定义选择") { _ in print("选择") }
+        let pase = UIAction(title: "自定义粘贴") { _ in print("粘贴") }
+        let edit = UIAction(title: "自定义编辑") { _ in print("编辑") }
+        let delete = UIAction(title: "自定义删除", attributes: .destructive) { _ in print("删除") }
+        let menu = UIMenu(children: [copy, select, pase, edit, delete])
         return menu
     }()
     
